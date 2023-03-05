9be2bc2847e3b88e63ecf8b918399271712f0dfa
Henri Gros <henrigros1@gmail.com>
2023-03-05 11:27:05 +0100

    Test suite V4

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NOCOLOR='\033[0m'
 
+
+# On compare le resultat des commandes entre notre shell et sh
 for test in tests/*.txt
 do
     ./sdriver.pl -t $test -s /bin/sh | sort > tests/default
@@ -12,16 +15,19 @@ do
     then
         echo -e ${GREEN}passed $test ${NOCOLOR}
     else
+        # On crée un fichier de log si le test échoue
         mv tests/tmp $test.log
         echo -e ${RED}failed $test ${NOCOLOR}
     fi
 done
 
+# On verifie que valgrind ne renvoie pas d'erreur sur nos tests
 for valgrind_test in tests/*.txt
 do
     ./sdriver.pl -t $valgrind_test -s /usr/bin/valgrind  -a "-q --leak-check=full --show-leak-kinds=all ./shell" 2> tests/output 1> /dev/null
     if [ -s tests/output ]; then
         echo -e ${RED}failed valgrind $valgrind_test ${NOCOLOR}
+        # On crée un fichier de log si le test échoue
         mv tests/output $valgrind_test.log
     else
         echo -e ${GREEN}passed valgrind $valgrind_test ${NOCOLOR}
